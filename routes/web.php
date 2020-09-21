6374850d3b934f2a56b245f236ecc2be02e6817e
sadaking <sichirigahama.37@gmail.com>
2020-09-21 14:39:38 +0900

    ルーティングの設定

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('contact/index', 'contactFormController@index');
 
-
-Route::resource('contacts', 'contactFormController')->only([
-]);
+Route::group(['prefix' => 'contact', 'middleware' => 'auth'], function(){
+  Route::get('index', 'contactFormController@index')->name('contact.index');
+  Route::get('create', 'contactFormController@create')->name('contact.create');
+  Route::post('store', 'contactFormController@store')->name('contact.store');
+  Route::get('show/{id}', 'contactFormController@show')->name('contact.show');
+  Route::get('edit/{id}', 'contactFormController@edit')->name('contact.edit');
+  Route::post('update/{id}', 'contactFormController@update')->name('contact.update');
+  Route::post('destroy/{id}', 'contactFormController@destroy')->name('contact.destroy');
+});
+// Route::resource('contacts', 'contactFormController')->only([
+// ]);
 
 Auth::routes();
 
